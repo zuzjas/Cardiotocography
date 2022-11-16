b61ba667ba8f30f697433f8fa7a9fc97948ed092
Zuzanna Jasinska <zuzanna.jasinska07@gmail.com>
2022-11-16 13:34:48 +0100

    Another way to sole missing values problem

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import pandas as pd
+
+
+# Fill missing values with column's mean
+def fill_missing_values(dataset, X):
+    X = X.fillna(value=X.mean())
+    return X
+
+
+# Delete missing values
+def delete_missing_values(dataset, X):
+    X_dropped = X.dropna(axis=0)
+    return X_dropped
+
+
 if __name__ == '__main__':
     # Load data with named columns
     dataset = pd.read_csv('ctg_3_col.csv')
 
-    # Fill missing values with column's mean
     X = dataset.drop(columns="15")
-    y = dataset["1"]
-    X = X.fillna(value=X.mean())
+    y = dataset["15"]
+
+    # Solve missing values problem
+    X = fill_missing_values(dataset,X)
+    #X = delete_missing_values(dataset, X)
 
     # Save result to csv
     dataset = X
